231a6f0319d4b4ea180f5ae5bc69a4591d3d42b8
Frank <frank@edynamics.co>
2020-11-21 15:37:47 -0500

    re-factored with IOT board class

--- lib/adxl362/src/adxl362.cpp
+++ lib/adxl362/src/adxl362.cpp
@@ -22,6 +22,8 @@
 
 //#define ADXL362_DEBUG
 
+#define SPI_PORT SPI1
+
 int16_t slaveSelectPin = SS;
 
 ADXL362::ADXL362() {
@@ -30,21 +32,22 @@ ADXL362::ADXL362() {
 
 //
 //  begin()
-//  Initial SPI setup, soft reset of device
+//  Initial SPI_PORT setup, soft reset of device
 //
 void ADXL362::begin(int16_t chipSelectPin) {
 	slaveSelectPin = chipSelectPin;
 	pinMode(slaveSelectPin, OUTPUT);
-	SPI.begin(SPI_MODE_MASTER);
-    //SPI.setBitOrder(MSBFIRST);
-    SPI.setClockSpeed(400, KHZ);
 
-	SPI.setDataMode(SPI_MODE0);	//CPHA = CPOL = 0    MODE = 0
+	SPI_PORT.begin(SPI_MODE_MASTER);
+    //SPI_PORT.setBitOrder(MSBFIRST);
+    SPI_PORT.setClockSpeed(1,MHZ);
+
+	SPI_PORT.setDataMode(SPI_MODE0);	//CPHA = CPOL = 0    MODE = 0
 	delay(1000);
     
 	// soft reset
 	SPIwriteOneRegister(0x1F, 0x52);  // Write to SOFT RESET, "R"
-	delay(500);
+	delay(10);
 #ifdef ADXL362_DEBUG
 	Serial.println("Soft Reset\n");
 #endif
@@ -124,19 +127,19 @@ int16_t ADXL362::readTemp(){
 }
 
 void ADXL362::readXYZTData(int16_t &XData, int16_t &YData, int16_t &ZData, int16_t &Temperature){
-	  // burst SPI read
+	  // burst SPI_PORT read
 	  // A burst read of all three axis is required to guarantee all measurements correspond to same sample time
 	  digitalWrite(slaveSelectPin, LOW);
-	  SPI.transfer(0x0B);  // read instruction
-	  SPI.transfer(0x0E);  // Start at XData Reg
-	  XData = SPI.transfer(0x00);
-	  XData = XData + (SPI.transfer(0x00) << 8);
-	  YData = SPI.transfer(0x00);
-	  YData = YData + (SPI.transfer(0x00) << 8);
-	  ZData = SPI.transfer(0x00);
-	  ZData = ZData + (SPI.transfer(0x00) << 8);
-	  Temperature = SPI.transfer(0x00);
-	  Temperature = Temperature + (SPI.transfer(0x00) << 8);
+	  SPI_PORT.transfer(0x0B);  // read instruction
+	  SPI_PORT.transfer(0x0E);  // Start at XData Reg
+	  XData = SPI_PORT.transfer(0x00);
+	  XData = XData + (SPI_PORT.transfer(0x00) << 8);
+	  YData = SPI_PORT.transfer(0x00);
+	  YData = YData + (SPI_PORT.transfer(0x00) << 8);
+	  ZData = SPI_PORT.transfer(0x00);
+	  ZData = ZData + (SPI_PORT.transfer(0x00) << 8);
+	  Temperature = SPI_PORT.transfer(0x00);
+	  Temperature = Temperature + (SPI_PORT.transfer(0x00) << 8);
 	  digitalWrite(slaveSelectPin, HIGH);
   
 #ifdef ADXL362_DEBUG
@@ -224,38 +227,38 @@ void ADXL362::checkAllControlRegs(){
 	//byte ODR = filterCntlReg & 0x07;  Serial.print("ODR = ");  Serial.println(ODR, HEX);
 	//byte ACT_INACT_CTL_Reg = SPIreadOneRegister(0x27);      Serial.print("ACT_INACT_CTL_Reg = "); Serial.println(ACT_INACT_CTL_Reg, HEX);
 	digitalWrite(slaveSelectPin, LOW);
-	SPI.transfer(0x0B);  // read instruction
-	SPI.transfer(0x20);  // Start burst read at Reg 20
+	SPI_PORT.transfer(0x0B);  // read instruction
+	SPI_PORT.transfer(0x20);  // Start burst read at Reg 20
 #ifdef ADXL362_DEBUG
 	Serial.println("Start Burst Read of all Control Regs - Library version 6-5-2014:");
-	Serial.print("Reg 20 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 21 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 22 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 23 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 24 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 25 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 26 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 27 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 28 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 29 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 2A = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 2B = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 2C = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 2D = "); 	Serial.println(SPI.transfer(0x00), HEX);
-	Serial.print("Reg 2E = "); 	Serial.println(SPI.transfer(0x00), HEX);
+	Serial.print("Reg 20 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 21 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 22 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 23 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 24 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 25 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 26 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 27 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 28 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 29 = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 2A = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 2B = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 2C = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 2D = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
+	Serial.print("Reg 2E = "); 	Serial.println(SPI_PORT.transfer(0x00), HEX);
 #endif
 	digitalWrite(slaveSelectPin, HIGH);
 }
 
-// Basic SPI routines to simplify code
+// Basic SPI_PORT routines to simplify code
 // read and write one register
 byte ADXL362::SPIreadOneRegister(byte regAddress){
 	byte regValue = 0;
   
 	digitalWrite(slaveSelectPin, LOW);
-	SPI.transfer(0x0B);  // read instruction
-	SPI.transfer(regAddress);
-	regValue = SPI.transfer(0x00);
+	SPI_PORT.transfer(0x0B);  // read instruction
+	SPI_PORT.transfer(regAddress);
+	regValue = SPI_PORT.transfer(0x00);
 	digitalWrite(slaveSelectPin, HIGH);
 
 	return regValue;
@@ -264,9 +267,9 @@ byte ADXL362::SPIreadOneRegister(byte regAddress){
 void ADXL362::SPIwriteOneRegister(byte regAddress, byte regValue){
   
 	digitalWrite(slaveSelectPin, LOW);
-	SPI.transfer(0x0A);  // write instruction
-	SPI.transfer(regAddress);
-	SPI.transfer(regValue);
+	SPI_PORT.transfer(0x0A);  // write instruction
+	SPI_PORT.transfer(regAddress);
+	SPI_PORT.transfer(regValue);
 	digitalWrite(slaveSelectPin, HIGH);
 }
 
@@ -274,10 +277,10 @@ int16_t ADXL362::SPIreadTwoRegisters(byte regAddress){
 	int16_t twoRegValue = 0;
   
 	digitalWrite(slaveSelectPin, LOW);
-	SPI.transfer(0x0B);  // read instruction
-	SPI.transfer(regAddress);  
-	twoRegValue = SPI.transfer(0x00);
-	twoRegValue = twoRegValue + (SPI.transfer(0x00) << 8);
+	SPI_PORT.transfer(0x0B);  // read instruction
+	SPI_PORT.transfer(regAddress);  
+	twoRegValue = SPI_PORT.transfer(0x00);
+	twoRegValue = twoRegValue + (SPI_PORT.transfer(0x00) << 8);
 	digitalWrite(slaveSelectPin, HIGH);
 
 	return twoRegValue;
@@ -288,9 +291,9 @@ void ADXL362::SPIwriteTwoRegisters(byte regAddress, int16_t twoRegValue){
 	byte twoRegValueL = twoRegValue;
   
 	digitalWrite(slaveSelectPin, LOW);
-	SPI.transfer(0x0A);  // write instruction
-	SPI.transfer(regAddress);  
-	SPI.transfer(twoRegValueL);
-	SPI.transfer(twoRegValueH);
+	SPI_PORT.transfer(0x0A);  // write instruction
+	SPI_PORT.transfer(regAddress);  
+	SPI_PORT.transfer(twoRegValueL);
+	SPI_PORT.transfer(twoRegValueH);
 	digitalWrite(slaveSelectPin, HIGH);
 }
--- src/project-4-tempsensor-wifi.ino
+++ src/project-4-tempsensor-wifi.ino
@@ -9,131 +9,71 @@ SYSTEM_THREAD(ENABLED);
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-#include "include/sensor.h"
-#include "include/display-oled.h"
-#include "include/serial-monitor.h"
-#include "include/net-wifi.h"
+#include "config.h"
+#include "IOTboard.h"
 
-#define BOARD_SYSTEM_LED                D7
+IOTboard iot_kit(true);
 
-#define SYSTEM_HEARTBEAT_TICK_MS        (250)
+enum app_state_t {
+    APP_STATE_INIT,
+    APP_STATE_NET_CONNECT,
+    APP_STATE_NET_WAIT,
+    APP_STATE_NET_READY
+};
 
-#define SYSTEM_DISPLAY_UPDATE_MS        (5000)
+static app_state_t _app_state = APP_STATE_INIT;
 
-#define WIFI_AP_NAME    "ececs"
-#define WIFI_AP_PASSW   "buckman203"
-
-typedef enum {
-    SYS_STATE_INIT,
-    SYS_STATE_NET_CONNECT,
-    SYS_STATE_NET_PENDING,
-    SYS_STATE_NET_READY
-}SystemState_t;
-
-static SystemState_t _systemState = SYS_STATE_INIT;
-
-// Temperature sensor Init and timer objects
-Timer timerSystemUIupdate(SYSTEM_DISPLAY_UPDATE_MS, SystemUIrefresh);
-
-// Display Average Temperature
-void SystemUIrefresh(void)
+void appStateMachine(void)
 {
-    // Get the average Temperature from the sensor task
-    // Update OLED display
-    DisplayOLEDupdateTemp(SensorTempGetAverage());
-}
-
-
-// System heartbeat 
-// Use software timer, this indicates state of the System
-Timer timerSysHeartbeat(SYSTEM_HEARTBEAT_TICK_MS, SystemHeartbeat);
-
-void SystemHeartbeat(void)
-{
-    static bool firstTime = true;
-    static bool ledState = true;
-
-    if (firstTime)
+    switch(_app_state)
     {
-        pinMode(BOARD_SYSTEM_LED, OUTPUT);
-        firstTime = false;
-        digitalWrite(BOARD_SYSTEM_LED, HIGH);
-    }
-    else
-    {
-        if (ledState)
-        {
-            digitalWrite(BOARD_SYSTEM_LED, LOW);
-            ledState = false;
-        }
-        else
-        {
-            digitalWrite(BOARD_SYSTEM_LED, HIGH);
-            ledState = true;
-        }
-    }
-}
+        case APP_STATE_INIT:
+            _app_state = APP_STATE_NET_CONNECT;
 
-void SystemStateProc(void)
-{
-    switch(_systemState)
-    {
-        case SYS_STATE_INIT:
-            _systemState = SYS_STATE_NET_CONNECT;
+            iot_kit.printToSerialOut("IOT app started");
             break;
 
-        case SYS_STATE_NET_CONNECT:
-            NetWifiStop();
-
+        case APP_STATE_NET_CONNECT:
             // Start WiFi connection
-            NetWifiStart(WIFI_AP_NAME, WIFI_AP_PASSW);
+            iot_kit.disconnectWiFi();
 
-            _systemState = SYS_STATE_NET_PENDING;
+            iot_kit.printToSerialOut("Connecting to %s...", WIFI_AP_SSID);
+
+            iot_kit.connectToWiFi(WIFI_AP_SSID, WIFI_AP_PASSW, IOT_WIFI_WPA2);
+
+            _app_state = APP_STATE_NET_WAIT;
             break;
 
-        case SYS_STATE_NET_PENDING:
+        case APP_STATE_NET_WAIT:
             // Check if we are connected to WiFi AP
-            if(NetWifiReady())
+            if (iot_kit.isWiFiReady())
             {
-                _systemState = SYS_STATE_NET_READY;
+                iot_kit.printToSerialOut("Connected!");
+
+                iot_kit.printToSerialOut("Local IP = %s", iot_kit.getWiFiIP());
+
+                _app_state = APP_STATE_NET_READY;
             }
             break;
 
-        case SYS_STATE_NET_READY:
+        case APP_STATE_NET_READY:
             // Connected to WiFi AP
             break;
     }
 
     // Run the Serial Monitor processor
     // Check for incoming commands from serial port
-    SerialMonitorInputProc();
 }
 
 // setup() runs once, when the device is first turned on.
 void setup()
 {
-    // Put initialization like pinMode and begin functions here.
-
-    // Start the Serial Monitor 
-    SerialMonitorInit(NULL);
-    
-    // Initialize OLED display
-    DisplayOLEDinit();
-    
-    // Start Sensor task
-    SensorTempInit();
-    
-    // Start UI refresh timer
-    // Update the display (OLED) every SYSTEM_DISPLAY_UPDATE_MS
-    timerSystemUIupdate.start();
-
-    // Start System heartbeat
-    timerSysHeartbeat.start();
+    iot_kit.start();
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop()
 {
     // The core of your code will likely live here.
-    SystemStateProc();
+    appStateMachine();
 }
